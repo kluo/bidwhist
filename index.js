1cdfa299a3e504cb9b63d88c52d1b2d12f6532c7
Kevin Luo <kevkluo@gmail.com>
2014-04-27 23:24:06 -0400

    Make port configurable

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 swig.setDefaults({cache: false});
 
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port %d', app.get('port'));
 });
