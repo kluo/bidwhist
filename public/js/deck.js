99e390faf1a6557eb6a3bdfc9e1d201a9da30b94
Kevin Luo <kevkluo@gmail.com>
2014-12-07 22:27:02 -0800

    Add deck shuffle method

--- public/js/deck.js
+++ public/js/deck.js
@@ -1,11 +1,23 @@
 function Deck() {
   this.cards = [];
   var suits = ['d', 'c', 'h', 's']
-  for (i = 0; i < suits.length; i++) {
-    for (rank = 1; rank <= 13; rank++) {
+  for (var i = 0; i < suits.length; i++) {
+    for (var rank = 1; rank <= 13; rank++) {
       this.cards.push(new Card(rank, suits[i]));
     }
   }
   this.cards.push(new Card(1, 'j'));
   this.cards.push(new Card(2, 'j'));
+}
+
+/**
+ * Fisher-Yates Shuffle Modern Algorithm
+ */
+Deck.prototype.shuffle = function() {
+  for (var i = this.cards.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = this.cards[i];
+    this.cards[i] = this.cards[j]
+    this.cards[j] = temp;
+  }
 }
