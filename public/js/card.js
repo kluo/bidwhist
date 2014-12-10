5b4ddb3d407a01597922ef94584a19bd72749a89
Kevin Luo <kevkluo@gmail.com>
2014-12-09 23:12:04 -0800

    Move suits array to Card type

--- public/js/card.js
+++ public/js/card.js
@@ -3,3 +3,5 @@ function Card(rank, suit) {
   this.suit = suit;
   this.isFaceUp = false;
 }
+
+Card.suits = ['d', 'c', 'h', 's']
--- public/js/deck.js
+++ public/js/deck.js
@@ -1,9 +1,8 @@
 function Deck() {
   this.cards = [];
-  var suits = ['d', 'c', 'h', 's']
-  for (var i = 0; i < suits.length; i++) {
+  for (var i = 0; i < Card.suits.length; i++) {
     for (var rank = 1; rank <= 13; rank++) {
-      this.cards.push(new Card(rank, suits[i]));
+      this.cards.push(new Card(rank, Card.suits[i]));
     }
   }
   this.cards.push(new Card(1, 'j'));
