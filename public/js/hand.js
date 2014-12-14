b87590b183158e3c0573c456b4fd5472ba440371
Kevin Luo <kevkluo@gmail.com>
2014-12-13 20:54:01 -0800

    Add removeCard method to Hand model

--- public/js/hand.js
+++ public/js/hand.js
@@ -9,3 +9,8 @@ Hand.prototype.addCard = function(card) {
 Hand.prototype.addCards = function(cards) {
   this.cards.concat(cards);
 }
+
+Hand.prototype.removeCard = function(card) {
+  var index = this.cards.indexOf(card);
+  this.cards.splice(index, 1);
+}
